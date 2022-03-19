48889241b41345f17aab0b968c8d7fa39c711416
sam-timmins <sam.timmins1@gmail.com>
2022-03-19 21:26:54 +0000

    Add social links to contexts

--- home/contexts.py
+++ home/contexts.py
@@ -3,10 +3,16 @@ def site_contexts(request):
 
     store_name = 'Swanbourne Village Stores'
     currency = '£'
+    facebook = 'https://www.facebook.com/SwanbourneStores'
+    instagram = 'https://www.instagram.com/'
+    twitter = 'https://www.twitter.com/'
 
     contexts = {
         'store_name': store_name,
         'currency': currency,
+        'facebook': facebook,
+        'instagram': instagram,
+        'twitter': twitter,
     }
 
     return contexts

2c9d0c9989f156d902e9b2f585dcda730d2f687f
sam-timmins <sam.timmins1@gmail.com>
2022-03-19 17:17:07 +0000

    Add currency to contexts

--- home/contexts.py
+++ home/contexts.py
@@ -2,9 +2,11 @@ def site_contexts(request):
     """ Site contexts variables """
 
     store_name = 'Swanbourne Village Stores'
+    currency = '£'
 
     contexts = {
         'store_name': store_name,
+        'currency': currency,
     }
 
     return contexts

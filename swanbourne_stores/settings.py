1ce4bddd28ac149e3df0abb6a8c6887f3475e154
sam-timmins <sam.timmins1@gmail.com>
2022-03-23 19:49:28 +0000

    Change secret key to be secure

--- swanbourne_stores/settings.py
+++ swanbourne_stores/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nohaa+8z3zhy088p9%pv+5$kcp#%s^9^p$)1uke0d$l5(xp3m5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

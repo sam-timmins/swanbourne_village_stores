780112890f29dcaae5818ab5b1d5d45bb4805b24
sam-timmins <sam.timmins1@gmail.com>
2022-03-25 20:43:07 +0000

    Add import of env

--- swanbourne_stores/settings.py
+++ swanbourne_stores/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 import dj_database_url
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -173,10 +176,11 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static and media file paths
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = ('cloudinary_storage.storage.'
+                       'StaticHashedCloudinaryStorage')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'

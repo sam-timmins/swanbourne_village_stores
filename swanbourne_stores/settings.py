0588cc69c4d7fb22290e5b853cf181c244403f01
sam-timmins <sam.timmins1@gmail.com>
2022-03-25 20:03:00 +0000

    Add cloudinary storage for static and media files

--- swanbourne_stores/settings.py
+++ swanbourne_stores/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -171,7 +173,10 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static and media file paths
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'

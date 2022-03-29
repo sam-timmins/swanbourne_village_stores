af845d6317e39dbf97fd90230d73648be159ad2a
sam-timmins <sam.timmins1@gmail.com>
2022-03-29 20:37:33 +0000

    Add send email functionalitly to index view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page
+    and send an email when  the contact form is submitted
+    """
 
-    return render(request, 'home/index.html')
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        send_mail(
+            f'Message from {message_name}',
+            message,
+            message_email,
+            ['sam.timmins1@gmail.com', ],
+            )
+
+        return render(request, 'home/index.html', {
+            'message_name': message_name,
+        })
+
+    else:
+        return render(request, 'home/index.html')

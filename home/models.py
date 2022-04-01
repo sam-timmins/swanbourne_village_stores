6bbb9d1418daa04d6fb3d5ffde55b574dbddf27c
sam-timmins <sam.timmins1@gmail.com>
2022-04-01 17:01:20 +0000

    Remove model for opening hours

--- home/models.py
+++ home/models.py
@@ -1,15 +1,1 @@
 from django.db import models
-
-
-class OpenHours(models.Model):
-    """ Opening hours for the store """
-
-    day = models.CharField(
-        max_length=50,
-        unique=True
-        )
-    open_time = models.TimeField()
-    close_time = models.TimeField()
-
-    def __str__(self):
-        return f'{self.day}'

3f766d6924da5d1ab6999d9fca644a24fb73a30f
sam-timmins <sam.timmins1@gmail.com>
2022-04-01 13:24:07 +0000

    Add model for opening hours

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class OpenHours(models.Model):
+    """ Opening hours for the store """
+
+    day = models.CharField(
+        max_length=50,
+        unique=True
+        )
+    open_time = models.TimeField()
+    close_time = models.TimeField()
+
+    def __str__(self):
+        return f'{self.day}'

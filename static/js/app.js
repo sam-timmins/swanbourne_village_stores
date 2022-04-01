7987df8f9594836d8def43d561ab82f02f0fb85c
sam-timmins <sam.timmins1@gmail.com>
2022-04-01 12:54:36 +0000

    Refactor and tidy code from base.html

---
+++ static/js/app.js
@@ -0,0 +1,21 @@
+const copyrightYearRef = document.querySelector("#copyright-year");
+
+window.onload = function () {
+  // Auto increment of the year in footer
+  const getFullYear = () =>
+    (copyrightYearRef.innerHTML = new Date().getFullYear());
+
+  getFullYear(); 
+
+  // Add slidedown animation to dropdown-menu
+  $(".dropdown").on("show.bs.dropdown", function (e) {
+    $(".dropdown-menu").removeClass("invisible");
+    $(this).find(".dropdown-menu").first().stop(true, true).slideDown();
+  });
+
+  // Add slideup animation to dropdown-menu
+  $(".dropdown").on("hide.bs.dropdown", function (e) {
+    $(this).find(".dropdown-menu").first().stop(true, true).slideUp();
+  });
+
+};

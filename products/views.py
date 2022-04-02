95d644df0e342812bed1bec5934baecbcc125be8
sam-timmins <sam.timmins1@gmail.com>
2022-04-02 14:40:31 +0000

    Add context to products view

--- products/views.py
+++ products/views.py
@@ -1,11 +1,23 @@
+from itertools import chain
 from django.shortcuts import render
+
+from .models import Dishes, Wines, Bundle
 
 
 def products(request):
     """ products view """
 
-    context = {
+    dishes = Dishes.objects.all()
+    wines = Wines.objects.all()
+    bundles = Bundle.objects.all()
 
+    all_products = list(chain(dishes, wines, bundles))
+
+    context = {
+        'dishes': dishes,
+        'wines': wines,
+        'bundles': bundles,
+        'all_products': all_products,
     }
 
     return render(

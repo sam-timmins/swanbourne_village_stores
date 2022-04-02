e60089974a2d8ec53bb7dd91b3e2cbc13d17608b
sam-timmins <sam.timmins1@gmail.com>
2022-04-02 13:50:49 +0000

    Add view for dishes category

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DishesCategory
+
+
+@admin.register(DishesCategory)
+class DishesCategoryAdmin(admin.ModelAdmin):
+    """ Settings for dishes categories in admin """
+    search_fields = ['name']
+    list_display = ('name', 'friendly_name')
+    list_filter = ('name', 'friendly_name')

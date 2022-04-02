5b01bc75a3ddd6cc9de21c194aa0b7590a853d5c
sam-timmins <sam.timmins1@gmail.com>
2022-04-02 15:39:49 +0000

    Add pagination to products view

--- products/views.py
+++ products/views.py
@@ -1,23 +1,30 @@
 from itertools import chain
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Dishes, Wines, Bundle
 
 
 def products(request):
-    """ products view """
+    """ Products view with pagination"""
 
     dishes = Dishes.objects.all()
     wines = Wines.objects.all()
     bundles = Bundle.objects.all()
 
-    all_products = list(chain(dishes, wines, bundles))
+    all_products = list(chain(dishes, wines))
+
+    paginator = Paginator(all_products, 24)
+
+    page_number = request.GET.get('page')
+    page_all_products = paginator.get_page(page_number)
 
     context = {
         'dishes': dishes,
         'wines': wines,
         'bundles': bundles,
         'all_products': all_products,
+        'page_all_products': page_all_products,
     }
 
     return render(

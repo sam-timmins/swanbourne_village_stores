edca890d0a1fe52f86f04cbd6dd978d55308d97f
sam-timmins <sam.timmins1@gmail.com>
2022-04-02 13:42:33 +0000

    Add model for dishes

--- products/models.py
+++ products/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+DISH_STATUS = ((0, 'Frozen'), (1, 'Fresh'))
 
 
 class DishesCategory(models.Model):
@@ -48,3 +51,44 @@ class WineCategory(models.Model):
     class Meta:
         """ Change verbose name in admin """
         verbose_name_plural = 'Wine Categories'
+
+
+class Dishes(models.Model):
+    """ Model for dishes """
+    category = models.ForeignKey(
+        'DishesCategory',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        )
+    name = models.CharField(
+        max_length=254,
+        unique=True,
+        )
+    slug_name = models.SlugField(
+        max_length=254,
+        unique=True,
+    )
+    status = models.IntegerField(
+        choices=DISH_STATUS,
+        default=1
+        )
+    image = CloudinaryField(
+        'image',
+        default='placeholder',
+        null=True,
+        blank=True,
+        )
+    description = models.TextField()
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        """ Change verbose name and ordering in admin """
+        verbose_name_plural = 'Dishes'
+        ordering = ['name']

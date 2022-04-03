d0005b0c88d8d2d8b0d2603dce86a5942289eca1
sam-timmins <sam.timmins1@gmail.com>
2022-04-03 11:50:24 +0000

    Add view for dishes product details

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('the_freezer/', views.the_freezer, name='the_freezer'),
     path('fresh_food/', views.fresh_food, name='fresh_food'),
     path('the_works/', views.the_works, name='the_works'),
+    path('<product_id>', views.product_detail_dishes, name='product_detail_dishes'),
 ]

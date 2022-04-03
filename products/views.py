cca0956c7042082269603ad53a901fedfc6d4c67
sam-timmins <sam.timmins1@gmail.com>
2022-04-03 09:20:35 +0000

    Add the view for the freezer

--- products/views.py
+++ products/views.py
@@ -50,3 +50,27 @@ def wine_store(request):
         'products/wine-store.html',
         context,
         )
+
+
+def the_freezer(request):
+    """ Menu view populating only the frozen foods in the dishes model with pagination """
+
+    dishes = Dishes.objects.all().filter(status=False)
+
+    paginator = Paginator(dishes, 24)
+
+    page_number = request.GET.get('page')
+    page_all_products = paginator.get_page(page_number)
+    number_of_pages = 'a' * page_all_products.paginator.num_pages
+
+    context = {
+        'dishes': dishes,
+        'page_all_products': page_all_products,
+        'number_of_pages': number_of_pages,
+    }
+
+    return render(
+        request,
+        'products/the-freezer.html',
+        context,
+        )

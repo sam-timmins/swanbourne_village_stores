551dada1f716b0648fbeb5cee704c1552ef504ee
sam-timmins <sam.timmins1@gmail.com>
2022-04-03 10:05:21 +0000

    Add url for the works

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('wine_store/', views.wine_store, name='wine_store'),
     path('the_freezer/', views.the_freezer, name='the_freezer'),
     path('fresh_food/', views.fresh_food, name='fresh_food'),
+    path('the_works/', views.the_works, name='the_works'),
 ]

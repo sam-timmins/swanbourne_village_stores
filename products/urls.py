fd7fb9da2975e8a585911bce9718cb00220f469a
sam-timmins <sam.timmins1@gmail.com>
2022-04-09 19:22:37 +0000

    Format for validation

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,44 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.the_menu, name='the_menu'),
-    path('wine_store/', views.wine_store, name='wine_store'),
-    path('the_freezer/', views.the_freezer, name='the_freezer'),
-    path('fresh_food/', views.fresh_food, name='fresh_food'),
-    path('the_works/', views.the_works, name='the_works'),
-    path('<product_id>', views.product_detail_dishes, name='product_detail_dishes'),
-    path('wine_store/<product_id>', views.product_details_wines, name='product_details_wines'),
-    path('the_works/<product_id>', views.product_details_bundles, name='product_details_bundles'),
+    path(
+        '',
+        views.the_menu,
+        name='the_menu'
+        ),
+    path(
+        'wine_store/',
+        views.wine_store,
+        name='wine_store'
+        ),
+    path(
+        'the_freezer/',
+        views.the_freezer,
+        name='the_freezer'
+        ),
+    path(
+        'fresh_food/',
+        views.fresh_food,
+        name='fresh_food'
+        ),
+    path(
+        'the_works/',
+        views.the_works,
+        name='the_works'
+        ),
+    path(
+        '<int:product_id>',
+        views.product_detail_dishes,
+        name='product_detail_dishes'
+        ),
+    path(
+        'wine_store/<int:product_id>',
+        views.product_details_wines,
+        name='product_details_wines'
+        ),
+    path(
+        'the_works/<int:product_id>',
+        views.product_details_bundles,
+        name='product_details_bundles'
+        ),
 ]

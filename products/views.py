5ed88c867bbf979615d6a174f553d19d4983f6da
sam-timmins <sam.timmins1@gmail.com>
2022-04-15 19:45:15 +0000

    Add view for editing a dish

--- products/views.py
+++ products/views.py
@@ -505,3 +505,45 @@ def delete__works_product(request, product_id):
 
     messages.success(request, 'The combination has been deleted from the store')
     return redirect(reverse('the_works'))
+
+
+def edit_dish(request, product_id):
+    """ Edit the item in the dishes model """
+    dishes = Dishes.objects.all()
+    product = get_object_or_404(Dishes, pk=product_id)
+
+    if request.method == 'POST':
+        form = DishForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated {product.name}')
+            return redirect(
+                reverse(
+                    'product_detail_dishes',
+                    args=[product.id],
+                    )
+                )
+        else:
+            messages.error(
+                request,
+                f'Unable to update {product.name}. \
+                Please ensure all fields are filled out correctly.'
+                )
+    else:
+        form = DishForm()
+
+    form = DishForm(instance=product)
+
+    messages.info(request, f'You are currently editing {product.name}')
+
+    context = {
+        'form': form,
+        'dishes': dishes,
+        'product': product,
+    }
+
+    return render(
+        request,
+        'products/edit-product.html',
+        context
+        )

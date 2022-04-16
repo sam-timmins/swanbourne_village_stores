ab2946dd2d1eb74f284be553cf2605271c2bcc27
sam-timmins <sam.timmins1@gmail.com>
2022-04-16 14:37:47 +0000

    Add validation to inputs and store correctly entered data to contact form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.contrib import messages
 
 
 def index(request):
@@ -10,17 +11,47 @@ def index(request):
     if request.method == "POST":
         message_name = request.POST['message-name']
         message_email = request.POST['message-email']
-        message = request.POST['message']
+        message_message = request.POST['message']
 
-        send_mail(
-            f'Message from {message_name}',
-            message,
-            message_email,
-            ['sam.timmins1@gmail.com', ],
-            )
+        name = message_name.strip()
+        message = message_message.strip()
+
+        if not message:
+            saved_name = name
+            saved_email = message_email
+
+            messages.error(
+                request,
+                'Please add a message so we can help.'
+                )
+            return render(request, 'home/index.html', {
+                'saved_name': saved_name,
+                'saved_email': saved_email,
+            })
+
+        elif not name:
+            saved_email = message_email
+            saved_message = message
+
+            messages.error(
+                request,
+                'Please enter your name.'
+                )
+            return render(request, 'home/index.html', {
+                'saved_message': saved_message,
+                'saved_email': saved_email,
+            })
+
+        else:
+            send_mail(
+                f'Message from {name}',
+                message_message,
+                message_email,
+                ['sam.timmins1@gmail.com', ],
+                )
 
         return render(request, 'home/index.html', {
-            'message_name': message_name,
+            'name': name,
         })
 
     else:

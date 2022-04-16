acdadc096006523e3a4b6b97aa543592515a521f
sam-timmins <sam.timmins1@gmail.com>
2022-04-16 20:05:48 +0000

    Change file name to test_views

--- products/tests/tests_views.py
+++ products/tests/tests_views.py
@@ -9,3 +9,12 @@ class TestTheMenuView(TestCase):
         """ Set up """
         self.client = Client()
         self.list_url = reverse('the_menu')
+
+
+    def test_the_menu_view_renders_correctly(self):
+        """ Test the menu view renders """
+
+        response = self.client.get(self.list_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/the-menu.html')

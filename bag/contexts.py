b57b299f906199577acd50536b2e3d5044720639
sam-timmins <sam.timmins1@gmail.com>
2022-04-17 15:46:03 +0000

    Add bag with session and context

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,9 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from itertools import chain
+
+from products.models import Dishes, Wines, Bundle
 
 
 def bag_contents(request):
@@ -14,6 +18,17 @@ def bag_contents(request):
     bag_items = []
     total = int(0)
     product_count = int(0)
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Dishes, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(

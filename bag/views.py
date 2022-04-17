6af58fee203ee5d3917cf3407d77710270fd99da
sam-timmins <sam.timmins1@gmail.com>
2022-04-17 15:45:33 +0000

    Change slug to id in add_to_bag

--- bag/views.py
+++ bag/views.py
@@ -7,18 +7,18 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 
-def add_to_bag(request, item_slug):
+def add_to_bag(request, item_id):
     """ Add a quantity of a product to the bag during a session """
 
     quantity = int(request.POST.get('item-quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
-    if item_slug in list(bag.keys()):
-        bag[item_slug] += quantity
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
     else:
-        bag[item_slug] = quantity
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
-
+    print(bag)
     return redirect(redirect_url)

c040bcc865cd1c5f5448d191f10e4647a3b167e2
sam-timmins <sam.timmins1@gmail.com>
2022-04-17 11:03:05 +0000

    Remove test content from view_bag

--- bag/views.py
+++ bag/views.py
@@ -4,14 +4,4 @@ from django.shortcuts import render
 def view_bag(request):
     """ A view to return the bag page """
 
-    bag_items = 'item'
-
-    context = {
-        'bag_items': bag_items,
-    }
-
-    return render(
-        request,
-        'bag/bag.html',
-        context
-        )
+    return render(request, 'bag/bag.html')

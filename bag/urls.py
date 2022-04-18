4cfa7f57cc3123ea6e702d5c78e7fc95e864a746
sam-timmins <sam.timmins1@gmail.com>
2022-04-18 14:38:37 +0000

    Add url for updating basket

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_slug>/', views.add_to_bag, name='add_to_bag'),
+    path('update/<item_slug>/', views.update_bag, name='update_bag'),
 ]

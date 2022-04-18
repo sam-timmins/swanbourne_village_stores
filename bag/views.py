33b55ad87571988558d434ddbc899e4db348bd2a
sam-timmins <sam.timmins1@gmail.com>
2022-04-18 14:38:54 +0000

    Add view for updating bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 
@@ -34,3 +34,27 @@ def add_to_bag(request, item_slug):
     request.session['bag'] = bag
 
     return redirect(redirect_url)
+
+
+def update_bag(request, item_slug):
+    """ Update the quantity of a product to the bag during a session """
+
+    if request.method == 'POST':
+        quantity = int(request.POST.get('item-quantity'))
+        product_name = request.POST.get('item_name')
+        bag = request.session.get('bag', {})
+
+        if item_slug in list(bag.keys()):
+            if quantity > 0:
+                bag[item_slug] = quantity
+                messages.success(
+                    request,
+                    f'Quantity of {product_name.title()} successfully \
+                        updated to {quantity}'
+                    )
+        else:
+            bag[item_slug] = quantity
+
+        request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))

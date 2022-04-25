e938bca41543d5f0246f41d2bd99741a749dc249
sam-timmins <sam.timmins1@gmail.com>
2022-04-25 20:21:41 +0000

    Add model for an order

--- checkout/models.py
+++ checkout/models.py
@@ -11,3 +11,84 @@ class CollectionDays(models.Model):
 
     def __str__(self):
         return f'{self.day}'
+
+
+class Order(models.Model):
+    """ Model for an order """
+
+    order_number = models.CharField(
+        max_length=32,
+        null=False,
+        editable=False
+        )
+    full_name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False
+        )
+    email = models.EmailField(
+        max_length=254,
+        null=False,
+        blank=False
+        )
+    phone_number = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False
+        )
+    collection_day = models.ForeignKey(
+        CollectionDays,
+        on_delete=models.PROTECT,
+        blank=False,
+        null=False,
+        )
+    country = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+        )
+    postcode = models.CharField(
+        max_length=20,
+        blank=True
+        )
+    town_or_city = models.CharField(
+        max_length=40,
+        null=False,
+        blank=False
+        )
+    street_address1 = models.CharField(
+        max_length=80,
+        null=False,
+        blank=False
+        )
+    street_address2 = models.CharField(
+        max_length=80,
+        blank=True
+        )
+    county = models.CharField(
+        max_length=80,
+        blank=True
+        )
+    date = models.DateTimeField(
+        auto_now_add=True
+        )
+    delivery_cost = models.DecimalField(
+        max_digits=6, decimal_places=2,
+        null=False,
+        default=0
+        )
+    order_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+        )
+    grand_total = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=False,
+        default=0
+        )
+
+    def __str__(self):
+        return f'{self.order_number}'

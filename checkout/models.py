e9c93ae7c3f4cc96e8e338437e29ace54f1bc9c9
sam-timmins <sam.timmins1@gmail.com>
2022-04-25 20:06:06 +0000

    Add model for collection days

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class CollectionDays(models.Model):
+    """ Model for colection days """
+    day = models.CharField(
+        max_length=9,
+        unique=True,
+    )
+
+    def __str__(self):
+        return f'{self.day}'

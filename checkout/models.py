a1bbf6e3b758145f49fa231cb1c18172945e8486
sam-timmins <sam.timmins1@gmail.com>
2022-04-25 20:51:03 +0000

    Add update total to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,8 @@
 import uuid
 
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
 
 from products.models import Dishes, Wines, Bundle
 
@@ -102,6 +104,19 @@ class Order(models.Model):
         Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a order item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.orderitems.aggregate(Sum('orderitem_total'))['orderitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
 
 class OrderItem(models.Model):

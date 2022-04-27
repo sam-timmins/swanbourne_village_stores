2eca1a259923a352122c000f868b928ea4b3de5b
sam-timmins <sam.timmins1@gmail.com>
2022-04-27 20:18:14 +0000

    Test string length of collection day

--- checkout/tests/test_models.py
+++ checkout/tests/test_models.py
@@ -16,3 +16,7 @@ class TestCollectionDayModel(TestCase):
         """ Test the collection day was created """
         count_days = CollectionDays.objects.all().count()
         self.assertEqual(count_days, 1)
+
+    def test_collection_day_string_length_is_less_than_or_equal_to_nine(self):
+        """ Test string length of collection day """
+        self.assertLessEqual(len(self.collection_day.day), 9)

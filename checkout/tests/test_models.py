2e82415e63cf2477b77ca590df5876ba3630741b
sam-timmins <sam.timmins1@gmail.com>
2022-04-27 20:15:04 +0000

    Add test to check setup worked

--- checkout/tests/test_models.py
+++ checkout/tests/test_models.py
@@ -11,3 +11,8 @@ class TestCollectionDayModel(TestCase):
         self.collection_day = CollectionDays.objects.create(
             day='Thursday'
         )
+
+    def test_collection_day_exists(self):
+        """ Test the collection day was created """
+        count_days = CollectionDays.objects.all().count()
+        self.assertEqual(count_days, 1)

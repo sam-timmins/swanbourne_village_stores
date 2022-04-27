e15a89f230fd142aaba45ae4e7ef299230a9f087
sam-timmins <sam.timmins1@gmail.com>
2022-04-27 20:43:37 +0000

    Add setup for tesing order model

--- checkout/tests/test_models.py
+++ checkout/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+import datetime
 
-from checkout.models import CollectionDays
+from checkout.models import CollectionDays, Order
 
 
 class TestCollectionDayModel(TestCase):
@@ -20,3 +21,33 @@ class TestCollectionDayModel(TestCase):
     def test_collection_day_string_length_is_less_than_or_equal_to_nine(self):
         """ Test string length of collection day """
         self.assertLessEqual(len(self.collection_day.day), 9)
+
+
+class TestOrderModel(TestCase):
+    """ Test the order model """
+
+    def setUp(self):
+        """ Model setup """
+        self.collection_day = CollectionDays.objects.create(
+            day='Thursday'
+        )
+
+        self.order_one = Order.objects.create(
+            order_number=12345678910,
+            full_name='Sam Timmins',
+            email='sam@email.com',
+            phone_number=1234567890,
+            collection_day=CollectionDays.objects.get(id=1),
+            country='Ireland',
+            postcode='A82E4T8',
+            town_or_city='Cavan',
+            street_address1='Main street',
+            street_address2='',
+            county='Cavan',
+            date=datetime.datetime.now(),
+            delivery_cost=10,
+            order_total=100,
+            grand_total=110,
+        )
+
+        

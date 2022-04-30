52cddfb33708359f76ab2e3bda78d3f56676a054
sam-timmins <sam.timmins1@gmail.com>
2022-04-30 18:51:35 +0000

    Add view for the checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
 
 
 def checkout(request):
-    """ A view to return the checkout page
     """
-    return render(request, 'checkout/checkout.html')
+    Checkout page view with order form it there are items in the
+    session and a error message if the bag is empty and the url
+    is forced
+    """
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, 'Your basket is currently empty')
+        return redirect(reverse('the_menu'))
+
+    order_form = OrderForm()
+
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, 'checkout/checkout.html', context)

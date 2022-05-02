8c871926d65ed54a6a6e701524bd1284537b3c49
sam-timmins <sam.timmins1@gmail.com>
2022-05-02 14:53:12 +0000

    Add check to prevent none error in update total in order model

--- checkout/models.py
+++ checkout/models.py
@@ -110,7 +110,7 @@ class Order(models.Model):
         Update grand total each time a order item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.orderitems.aggregate(Sum('orderitem_total'))['orderitem_total__sum']
+        self.order_total = self.orderitems.aggregate(Sum('orderitem_total'))['orderitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:

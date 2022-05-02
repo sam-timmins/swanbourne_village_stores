486a7d2033f556c7c585829d9ff3aa1121115297
sam-timmins <sam.timmins1@gmail.com>
2022-05-02 10:50:13 +0000

    Add stripe setup

--- static/js/checkout/checkout.js
+++ static/js/checkout/checkout.js
@@ -1,1 +1,27 @@
-console.log('test the checkout file');
+// Stripe setup
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#482832',
+        backgroundColor: '#FFFFFF',
+        fontFamily: '"Jost", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            background: '#191919',
+            opacity: '0.7',
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {
+    style: style,
+    hidePostalCode: true,
+});
+card.mount('#card-element');

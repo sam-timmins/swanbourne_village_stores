65cde15e3c57c27330038956d12900b6b41b1d5c
sam-timmins <sam.timmins1@gmail.com>
2022-05-02 12:03:31 +0000

    Add instance of form using form post data

--- checkout/views.py
+++ checkout/views.py
@@ -17,27 +17,47 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, 'Your basket is currently empty')
-        return redirect(reverse('the_menu'))
+    if request.method == 'POST':
+        bag = request.session.get('bag', {})
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    stripe_total = round(total * 100)
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'collection_day': request.POST['collection_day'],
+            # 'country': request.POST['country'],
+            # 'postcode': request.POST['postcode'],
+            # 'town_or_city': request.POST['town_or_city'],
+            # 'street_address1': request.POST['street_address1'],
+            # 'street_address2': request.POST['street_address2'],
+            # 'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
 
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-        )
+        print(form_data)
 
-    order_form = OrderForm()
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, 'Your basket is currently empty')
+            return redirect(reverse('the_menu'))
 
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        stripe_total = round(total * 100)
 
-    }
-    return render(request, 'checkout/checkout.html', context)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+            )
+
+        order_form = OrderForm()
+
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+
+        }
+        return render(request, 'checkout/checkout.html', context)

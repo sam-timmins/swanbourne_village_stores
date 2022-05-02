f8d5c966e894ef64ff17dbc1cc9d7b89f913e0e5
sam-timmins <sam.timmins1@gmail.com>
2022-05-02 14:52:20 +0000

    Add orders to admin page

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import CollectionDays
+from .models import CollectionDays, Order
 
 
 @admin.register(CollectionDays)
@@ -9,3 +9,16 @@ class CollectionDaysAdmin(admin.ModelAdmin):
     search_fields = ['day']
     list_display = ('day',)
     list_filter = ('day',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Settings for orders in admin """
+    list_display = (
+        'date',
+        'order_number',
+        'full_name',
+        'email',
+        'phone_number',
+        'collection_day'
+        )

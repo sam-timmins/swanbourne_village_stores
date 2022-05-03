8b9ac87f2f785f48bff15b528060336d2c2b1436
sam-timmins <sam.timmins1@gmail.com>
2022-05-03 20:36:53 +0000

    Add method for payment intent succeeded

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 class StripeWebhookHandler:
     """ Handle Stripe webhooks """
-    
+
     def __init__(self, request):
         self.request = request
 
@@ -13,4 +13,12 @@ class StripeWebhookHandler:
         """
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment intent succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
             status=200)

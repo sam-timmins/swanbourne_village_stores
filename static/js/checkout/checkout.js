0b614c8d2f017135b037f7be0af8fd29aaea006e
sam-timmins <sam.timmins1@gmail.com>
2022-05-04 19:58:51 +0000

    Fix typo in post function

--- static/js/checkout/checkout.js
+++ static/js/checkout/checkout.js
@@ -62,48 +62,38 @@ form.addEventListener('submit', function(ev) {
     };
     var url = '/checkout/cache_checkout_data/';
 
-    $post(url, postData).done(function() {
+    $.post(url, postData).done(function () {
     // Provide card to Stripe
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
-                name: $.trim(form.full_name.value),
-                phone: $.trim(form.phone_number.value),
-                email: $.trim(form.email.value),
-                address:{
-                    line1: $.trim(form.street_address1.value),
-                    line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.town_or_city.value),
-                    postcode: $.trim(form.postcode.value),
-                    country: $.trim(form.country.value),
-                    state: $.trim(form.county.value),
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                }
+            },
+        }).then(function(result) {
+        // Show error message if there are errors
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fa-solid fa-bomb me-2"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
                 }
             }
-        }
-    }).then(function(result) {
-        // Show error message if there are errors
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                    <i class="fa-solid fa-bomb me-2"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            // Submit form if payment intent succeeded
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    }).fail(function() {
+        });
+    }).fail(function () {
         location.reload();
     });
-    });
-
 });

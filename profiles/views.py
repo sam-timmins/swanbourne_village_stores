bf13fc34ab2ddde919b1ac85e09dfc2253810687
sam-timmins <sam.timmins1@gmail.com>
2022-05-07 19:38:19 +0000

    Add to profile view form valid check and prefill form

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """
     Profile view
     """
-    context = {}
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Thanks {user_profile}, \
+                your details have been updated.')
+
+    form = UserProfileForm(instance=user_profile)
+    orders = user_profile.orders.all()
+
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, 'profiles/profile.html', context)

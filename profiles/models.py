6aae5d202fd7bee6be65a5c19a10438fa53eba08
sam-timmins <sam.timmins1@gmail.com>
2022-05-07 13:14:40 +0000

    Add user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+        )
+
+    def __str__(self):
+        return self.user.username

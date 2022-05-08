96f15b067e044eb6c31ea8f0a430671f817ea2db
sam-timmins <sam.timmins1@gmail.com>
2022-05-08 13:41:41 +0000

    Add render of orders to the context in orders

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from checkout.models import Order
+
 
 def orders(request):
     """ A view to return the orders
     """
-    return render(request, 'orders/orders.html')
+    orders = Order.objects.all().order_by('-date')
+
+    context = {
+        'orders': orders,
+        }
+    return render(request, 'orders/orders.html', context)

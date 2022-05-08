cb18942c1f6155624d1a56b340659f40139164d6
sam-timmins <sam.timmins1@gmail.com>
2022-05-08 13:17:05 +0000

    Add basic view for orders for testing

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def orders(request):
+    """ A view to return the orders
+    """
+    return render(request, 'orders/orders.html')

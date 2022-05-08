c16a313eb7939820733a5ff44c6381a600c4d1e2
sam-timmins <sam.timmins1@gmail.com>
2022-05-08 18:21:08 +0000

    Add url for order details

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.orders, name='orders'),
+    path('order_details/<order_number>',
+         views.order_details,
+         name='order_details'
+         ),
 ]

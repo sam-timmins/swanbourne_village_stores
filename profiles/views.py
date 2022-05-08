0682170fb663b3806ea03b9599c352b3f9e5889f
sam-timmins <sam.timmins1@gmail.com>
2022-05-08 09:58:55 +0000

    Remove message from order_history

--- profiles/views.py
+++ profiles/views.py
@@ -36,11 +36,6 @@ def order_history(request, order_number):
 
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. \
-        A confirmation email was sent on the order date.'
-    ))
-
     context = {
         'order': order,
         'from_profile': True,

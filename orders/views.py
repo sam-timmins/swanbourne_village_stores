d43b52f2947674093d3c7024383daa084caac942
sam-timmins <sam.timmins1@gmail.com>
2022-05-09 19:10:18 +0000

    Add update status form to order_details

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 from checkout.models import Order
+
+from .forms import UpdateStatusForm
 
 
 def orders(request):
@@ -19,8 +21,30 @@ def order_details(request, order_number):
 
     order = get_object_or_404(Order, order_number=order_number)
 
+    form = UpdateStatusForm(data=request.POST)
+
+    if form.is_valid():
+
+        update = form.save(commit=False)
+
+        status = request.POST['status']
+
+        update.order = order
+        order.status = status
+        order.save()
+        messages.success(request, 'Order successfully updated')
+
+        return redirect(reverse(
+            'order_details',
+            args=[order.order_number])
+            )
+
+    else:
+        form = UpdateStatusForm()
+
     context = {
         'order': order,
+        'form': form,
     }
 
     return render(request, 'orders/orders_details.html', context)

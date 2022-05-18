4228241e3fd911aa6bc514dfff9121fd7eaa2950
sam-timmins <sam.timmins1@gmail.com>
2022-05-18 19:25:40 +0000

    Add view for administration page

--- administration/views.py
+++ administration/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def administration(request):
+    """ A view to return the privacy policy """
+
+    return render(request, 'administration/administration.html')

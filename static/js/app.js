dec4629aebdd346f5295e33ba13cd8e1c650ff1f
sam-timmins <sam.timmins1@gmail.com>
2022-05-21 19:36:52 +0000

    Add script to enable buttons when the document is loaded

--- static/js/app.js
+++ static/js/app.js
@@ -19,10 +19,17 @@ const copyrightYearRef = document.querySelector("#copyright-year");
   });
 
 
-  // Bootstrap
+ 
   $(document).ready(function() {
+    // Bootstrap
     // Show the toasts
     $(".toast").toast('show');
     // Show the tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+
+    // Remove disabled class from buttons
+    $('.btn').removeClass('disabled');
   });
+
+

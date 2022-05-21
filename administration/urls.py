9bbbccf3f55e4436525b6755b4f4faf260a8dbbe
sam-timmins <sam.timmins1@gmail.com>
2022-05-21 07:50:19 +0000

    Add url for deleting a dish

--- administration/urls.py
+++ administration/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
         views.dishes,
         name='dishes'
     ),
+    path(
+        'delete/dish/<int:dish_id>',
+        views.delete_dish_product,
+        name='delete_dish_product'
+    ),
 ]

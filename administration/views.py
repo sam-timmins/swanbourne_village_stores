b5071b785b4db23ed87fc7066508e7cb070d636c
sam-timmins <sam.timmins1@gmail.com>
2022-05-21 14:20:10 +0000

    Add collection days views for view all and delete

--- administration/views.py
+++ administration/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from products.models import Dishes, Wines, Bundle, DishesCategory, WineCategory
+from checkout.models import Order, CollectionDays
+
+from orders.forms import CreateCollectionDayForm
 
 from .forms import (DishForm, WineForm, WorksForm, DishCategoryForm,
                     WineCategoryForm)
@@ -534,3 +537,65 @@ def edit_wine_category(request, category_id):
         'components/categories/edit-wine-categories.html',
         context
         )
+
+
+@login_required
+def collection_days(request):
+    """ A view to return the collection days with the ability to
+    create a new collection day on a post request
+    """
+    if not request.user.is_superuser:
+        messages.info(request, 'Only store owners can view the \
+            collection day admin')
+        return redirect(reverse('home'))
+
+    days = CollectionDays.objects.all()
+    form = CreateCollectionDayForm()
+
+    if request.method == 'POST':
+        form = CreateCollectionDayForm(request.POST)
+        if form.is_valid():
+            day = form.cleaned_data.get('day')
+            form.save()
+            messages.success(request, f'Successfully added {day} to the \
+                available collection days')
+            return redirect(reverse('collection_days'))
+
+    context = {
+        'days': days,
+        'form': form,
+        }
+
+    return render(request, 'administration/collection-days.html', context)
+
+
+@login_required
+def delete_collection_day(request, pk):
+    """
+    Deletes a collection day based on the pk if there are
+    no open orders against the day
+    """
+    if not request.user.is_superuser:
+        messages.info(request, 'Only store owners can delete a \
+            collection day')
+        return redirect(reverse('home'))
+
+    day = CollectionDays.objects.get(pk=pk)
+    all_orders = Order.objects.filter(status=0)
+
+    day = str(day).lower()
+
+    order_list = []
+    for order in all_orders:
+        collection_day = str(order.collection_day).lower()
+        order_list.append(collection_day)
+
+    if day in order_list:
+        messages.error(request, 'There are open orders against. \
+            Please complete all orders due and then delete')
+        return redirect('collection_days')
+    else:
+        day = CollectionDays.objects.get(pk=pk)
+        day.delete()
+        messages.success(request, 'Collection day successfully deleted.')
+        return redirect('collection_days')

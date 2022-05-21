a38b10c47e0d0201968b0e4be611ed0c4c5789e1
sam-timmins <sam.timmins1@gmail.com>
2022-05-21 07:52:54 +0000

    Add edit dish url

--- administration/urls.py
+++ administration/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
         views.delete_dish_product,
         name='delete_dish_product'
     ),
+    path(
+        'edit-dish/<int:dish_id>',
+        views.edit_dish,
+        name='edit_dish'
+        ),
 ]

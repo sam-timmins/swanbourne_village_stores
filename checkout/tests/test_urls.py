1a5f55cf8e149fa46f2e7c09f2a47fbbae87177e
sam-timmins <sam.timmins1@gmail.com>
2022-05-28 14:35:30 +0000

    Add test for cache checkout data url

--- checkout/tests/test_urls.py
+++ checkout/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from checkout.views import checkout, checkout_success
+from checkout.views import checkout, checkout_success, cache_checkout_data
 
 
 class TestUrls(TestCase):
@@ -15,3 +15,8 @@ class TestUrls(TestCase):
         """ Test the response of the checkout success URL """
         url = reverse('checkout_success', args=['THD3591GER'])
         self.assertEqual(resolve(url).func, checkout_success)
+
+    def test_response_of_cache_checkout_data(self):
+        """ Test the response of the cache checkout data URL """
+        url = reverse('cache_checkout_data')
+        self.assertEqual(resolve(url).func, cache_checkout_data)

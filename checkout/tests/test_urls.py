0e2bd4b590ed6e5684adcee09c41f001ff3a08dc
sam-timmins <sam.timmins1@gmail.com>
2022-05-28 14:34:36 +0000

    Add test for checkout success url

--- checkout/tests/test_urls.py
+++ checkout/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from checkout.views import checkout
+from checkout.views import checkout, checkout_success
 
 
 class TestUrls(TestCase):
@@ -10,3 +10,8 @@ class TestUrls(TestCase):
         """ Test the response of the checkout URL """
         url = reverse('checkout')
         self.assertEqual(resolve(url).func, checkout)
+
+    def test_response_of_checkout_success_page(self):
+        """ Test the response of the checkout success URL """
+        url = reverse('checkout_success', args=['THD3591GER'])
+        self.assertEqual(resolve(url).func, checkout_success)
